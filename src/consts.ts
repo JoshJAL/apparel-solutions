8918a54695181859902f4164c4e22079cb913cc4
Joshua Levine <joshuaalevine3@gmail.com>
2023-03-22 23:15:43 -0400

    add dark and light mode, bring in custom font, add blurb compoenent create main and contnet sections as well as head component

---
+++ src/consts.ts
@@ -0,0 +1,11 @@
+export const SITE_TITLE = 'Apparel Solutions';
+
+export const SITE_DESCRIPTION =
+  "Apparel Solutions is the US Sales arm for our Vietnam factories. Men's and women's wearing apparel of all fabrication and makes";
+
+export const MICHAEL_EMAIL = 'Michael.Levine@apparel-solution.com';
+
+export const MICAHEL_PHONE = '813-579-5736';
+export const CHAU_EMAIL = 'Chau.Truong@apparel-solution.com';
+
+export const CHAU_PHONE = '+84 9 3532 6684';
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        backgroundLight: 'rgb(248, 242, 218)',
+        backgroundLightButtons: 'rgb(250, 215, 72)'
+      },
+      fontFamily: {
+        seaside: ['Seaside Resort NF']
+      }
+    }
+  },
+  plugins: []
+};
